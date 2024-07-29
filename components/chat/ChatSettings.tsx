280a693345e36f37ff92d38a0ef66cf19728f6cb
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-07-29 00:49:44 -0400

    feature(): update design to show db design

--- components/chat/ChatSettings.tsx
+++ components/chat/ChatSettings.tsx
@@ -1,5 +1,11 @@
-export function ChatSettings() {
+type Props = {
+  dbDesign: object | null
+}
+
+export function ChatSettings({ dbDesign }: Props) {
   return (
-    <aside className='grid w-full items-start gap-6' />
+    <aside className='grid w-full items-start gap-6'>
+      {JSON.stringify(dbDesign, null, 3)}
+    </aside>
   )
 }
--- screens/ChatScreen.tsx
+++ screens/ChatScreen.tsx
@@ -1,35 +1,64 @@
 'use client'
-import { ChatHistory, ChatInputMessage, ChatSettings } from '@/components/chat'
+
+import { useEffect, useState } from 'react'
 import { useChat } from 'ai/react'
+import { ChatHistory, ChatInputMessage, ChatSettings } from '@/components/chat'
+import { DbDesign } from '@/lib/ai'
 
 export function ChatScreen() {
-  const { messages, isLoading, input, setInput, append } = useChat({
+  const [isLoading, setLoading] = useState(false)
+  const [dbDesign, setDbDesign] = useState<object | null>(null)
+  const { messages, isLoading: chatIsLoading, input, setInput, reload, setMessages } = useChat({
     keepLastMessageOnError: true
   })
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      setMessages([{
+        id: crypto.randomUUID(),
+        role: 'system',
+        content: 'Hola, cuéntame cuales son los requerimientos de tu aplicación y te ayudaré a modelar una base de datos para tí.'
+      }])
+    }
+  }, [messages.length, setMessages])
 
   const handleInputChange = (text: string) => {
     setInput(text)
   }
 
-  const handleSubmit = () => {
-    append({
-      role: 'user',
-      content: input
+  const handleSubmit = async () => {
+    setLoading(true)
+    setInput('')
+    setMessages([...messages, { id: crypto.randomUUID(), role: 'user', content: input }])
+    const response = await fetch('/api/object', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ input })
     })
+
+    // const dbDesignResponse: DbDesign = JSON.parse(await response.text())
+    const { design }: DbDesign = await response.json()
+    setDbDesign(design)
+    await reload()
+    setLoading(false)
   }
 
   return (
     <main className='grid flex-1 gap-4 overflow-auto p-4 md:grid-cols-2'>
       {/* Chat Settings */}
       <div className='relative hidden flex-col items-start gap-8 md:flex'>
-        <ChatSettings />
+        <ChatSettings
+          dbDesign={dbDesign}
+        />
       </div>
 
       <div className='relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4'>
         {/* Chat History */}
         <ChatHistory
           messages={messages}
-          isLoading={isLoading}
+          isLoading={isLoading || chatIsLoading}
         />
 
         {/* Box Input Messages */}
