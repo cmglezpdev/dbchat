53b052f96f29a18e371c95e3086e16da7d08aa1b
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-08-02 19:06:45 -0400

    type(): add types

--- types.d.ts
+++ types.d.ts
@@ -3,3 +3,17 @@ export type ChatMessage = {
   role: 'user' | 'system',
   content: string
 }
+
+export type DbSchema = {
+  name: string;
+  attributes: string[];
+  primary_keys: string[];
+  foreign_keys: {
+    id: string;
+    reference: string
+  }[];
+}
+
+export type DbDesign = {
+  design: DbSchema[];
+}
