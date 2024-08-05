d2549302aa14ce349c3ec8039a841fa93110957b
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-08-04 22:31:44 -0400

    feature(): add new icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,3 +1,9 @@
 export * from './DatabaseOff'
 export * from './TableOff'
-export * from './openai'
+export * from './OpenAI'
+
+export * from './MariaDB'
+export * from './PostgresSQL'
+export * from './MySQL'
+export * from './SQLite'
+export * from './MySqlServer'
